a11fc794d2ea61a080a20a3b8b9f6e1f3968bffb
Marco Rolon <mrolon@mebilor.com.uy>
2020-04-17 14:13:44 -0300

    Version 1.0 de datalogger

--- src/datalogger.cpp
+++ src/datalogger.cpp
@@ -1,28 +1,137 @@
 #include "datalogger.h"
 #include "control.h"
+#include "user_interface.h"
+
+bool logEnable;
+bool printUserSettings;
+
+uint32_t logTimeoutMillis;
+
 
 void DataLogger_Init()
 {
   Serial.begin(115200);
+
+  logEnable = true;
+  printUserSettings = false;
+
+  logTimeoutMillis = DATALOG_STATUS_TIMEOUT;
+
+  // Initial message
+  Serial.println((String)"<Respirone, " + (String)"Version "+String(FIRMWARE_VERSION_HIGH)+"." +String(FIRMWARE_VERSION_LOW) + (String)", "+ (String)__DATE__ + (String)">\n\n");
 }
 
 void DataLogger_Task()
 {
-  static uint32_t initialMillis;
-
-  if((millis() - initialMillis) > 200)
+  // Log report
+  if (logEnable)
   {
-    DataLogger_ReportStatus();
-    initialMillis = millis();
+    static uint32_t initialMillis;
+
+    if((millis() - initialMillis) > logTimeoutMillis)
+    {
+      initialMillis = millis();
+      DataLogger_ReportStatus();  
+    }
+  }
+
+  // User settings
+  if (printUserSettings)
+  {
+    printUserSettings = false;
+    
+    DataLogger_PrintUserSettings();
   }
 }
 
 void DataLogger_ReportStatus()
 {
+  Serial.print("<BpM="+String(CTRL.breathsMinute)+",");   
+  Serial.print("Pr="+String(CTRL.pressure)+",");           
+  Serial.print("Ppk="+String(CTRL.peakPressure)+",");      
+  Serial.print("Ppl="+String(CTRL.plateauPressure)+",");   
+  Serial.print("Ppe="+String(CTRL.PEEP)+",");              
+  Serial.print("Vr="+String(CTRL.volume)+",");            
+  Serial.print("VpM="+String(CTRL.volumeMinute)+">\n");  
+}
 
+void DataLogger_PrintUserSettings()
+{
+  switch (UI.selectedMode)
+  {
+    case UI_VOLUME_CONTROL:
+      Serial.print("<MS=Vlm,");
+      
+      Serial.print("IE="+String(UI.i_e)+",");  
+  
+      Serial.print("BM="+String(UI.breathsMinute)+",");    
+      Serial.print("BMM="+String(UI.maxBreathsMinute)+",");      
+      Serial.print("BMm="+String(UI.minBreathsMinute)+",");  
+      
+      Serial.print("PA="+String(UI.adjustedPressure)+",");    
+      Serial.print("PM="+String(UI.maxPressure)+",");  
+      Serial.print("Pm="+String(UI.minPressure)+",");  
+      
+      Serial.print("VT="+String(UI.tidalVolume)+",");            
+      Serial.print("VMM="+String(UI.maxVolumeMinute)+",");   
+      Serial.print("VMm="+String(UI.minVolumeMinute)+",");
+
+      Serial.print("TrP="+String(UI.TrP)+">\n");
+      break;
+
+    case UI_PRESSURE_CONTROL:
+      Serial.print("<MS=Prs,");
+
+      Serial.print("IE="+String(UI.i_e)+",");  
+  
+      Serial.print("BM="+String(UI.breathsMinute)+",");    
+      Serial.print("BMM="+String(UI.maxBreathsMinute)+",");      
+      Serial.print("BMm="+String(UI.minBreathsMinute)+",");  
+      
+      Serial.print("PA="+String(UI.adjustedPressure)+",");    
+      Serial.print("PM="+String(UI.maxPressure)+",");  
+      Serial.print("Pm="+String(UI.minPressure)+",");  
+      
+      Serial.print("VT="+String(UI.tidalVolume)+",");            
+      Serial.print("VMM="+String(UI.maxVolumeMinute)+",");   
+      Serial.print("VMm="+String(UI.minVolumeMinute)+",");
+
+      Serial.print("TrP="+String(UI.TrP)+">\n");
+
+      break;
+    
+    default:
+      // ToDo> error 
+      break;
+  }
 }
 
 void DataLogger_ReportError(uint8_t errorNumber)
 {
 
+}
+
+void serialEvent() 
+{
+  while (Serial.available()) 
+  {
+    switch ((char)Serial.read())
+    {
+      case 'l': case 'L':
+        logEnable^=true;
+        break;
+
+      case 'u': case 'U':
+        printUserSettings=true;  
+        break;    
+
+      case '+':
+        logTimeoutMillis += 50; 
+        break; 
+
+      case '-':
+        (logTimeoutMillis>50)? (logTimeoutMillis -= 50) : logTimeoutMillis = 0; 
+        break; 
+    }
+  }
 }
